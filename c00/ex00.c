4a272989c07d752427a9704858111d9ca6583575
Tidian Delage <tdelage@2F5.42angouleme.fr>
2023-08-17 20:14:59 +0200

    creating file for c00ex00 test

---
+++ c00/ex00.c
@@ -0,0 +1,27 @@
+extern void ft_putchar(char);
+
+#include <stdio.h>
+
+int main() {
+
+	// test 01
+	printf("====== TEST ft_div_mod(01) ======\n");
+	printf("affichage attendu :\n\tC\n");
+	printf("affichage obtenue :\n\t");
+	ft_putchar('C');
+	ft_putchar('\n');
+
+	// test 02
+	printf("====== TEST ft_div_mod(02) ======\n");
+
+
+	// test 03
+	printf("====== TEST ft_div_mod(03) ======\n");
+
+	// test 04
+	printf("====== TEST ft_div_mod(04) ======\n");
+
+	// end for tests
+	printf("========= FIN DES TESTS =========\n");
+	return 0;
+}
