063aaf86825c3fbbce59a4a945386b916b4404a1
Tidian Delage <tdelage@2C7.42angouleme.fr>
2023-08-19 04:45:33 +0200

    dding test 01 (to be modified) and remove \t from test03

--- c01/test03.c
+++ c01/test03.c
@@ -3,35 +3,31 @@ extern void ft_div_mod(int, int, int*, int*);
 #include <stdio.h>
 
 int main() {
-	int div, mod, tot = 0;
+	int div, mod;
 
 	// test 01
 	printf("\033[96m====== TEST ft_div_mod(01) ======\033[39m\n");
 	ft_div_mod(12, 6, &div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\n\tdiv = 2\n\tmod = 0\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\n\tdiv = %d\n\tmod = %d\n", div, mod);
-	if(div == 2 && mod == 0) tot++;
+	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 2\nmod = 0\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
 
 	// test 02
 	printf("\033[96m====== TEST ft_div_mod(02) ======\033[39m\n");
 	ft_div_mod(15, 8, &div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\n\tdiv = 1\n\tmod = 7\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\n\tdiv = %d\n\tmod = %d\n", div, mod);
-	if(div == 1 && mod == 7) tot++;
+	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 1\nmod = 7\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
 
 	// test 03
 	printf("\033[96m====== TEST ft_div_mod(03) ======\033[39m\n");
 	ft_div_mod(323, 100, &div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\n\tdiv = 3\n\tmod = 0\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\n\tdiv = %d\n\tmod = %d\n", div, mod);
-	if(div == 3 && mod == 23) tot++;
+	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 3\nmod = 23\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
 
 	// test 04
 	printf("\033[96m====== TEST ft_div_mod(04) ======\033[39m\n");
 	ft_div_mod(130, 3, &div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\n\tdiv = 43\n\tmod = 1\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\n\tdiv = %d\n\tmod = %d\n", div, mod);
-	if(div == 43 && mod == 1) tot++;
+	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 43\nmod = 1\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
 
 	// end for tests
 	printf("\033[96m========= FIN DES TESTS =========\033[39m\n");
