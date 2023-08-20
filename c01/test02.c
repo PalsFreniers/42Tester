e5fcd47da08c5f0ff5ef64d301d5e7eda1d2d6b4
Strong214356 <tidiandelage1@gmail.com>
2023-08-20 02:42:27 +0200

    modif c01/swap var names for better understanding

--- c01/test02.c
+++ c01/test02.c
@@ -3,35 +3,35 @@ extern void ft_swap(int*, int*);
 #include <stdio.h>
 
 int main() {
-	int div, mod;
+	int a, b;
 
 	// test 01
 	printf("\033[96m====== TEST ft_swap(01) ======\033[39m\n");
-	div = 0; mod = 2;
-	ft_swap(&div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 2\nmod = 0\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
+	a = 0; b = 2;
+	ft_swap(&a, &b);
+	printf("\033[36mvaleurs attendue :\033[39m\na = 2\nb = 0\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\na = %d\nb = %d\n", a, b);
 
 	// test 02
 	printf("\033[96m====== TEST ft_swap(02) ======\033[39m\n");
-	div = 7; mod = 1;
-	ft_swap(&div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 1\nmod = 7\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
+	a = 7; b = 1;
+	ft_swap(&a, &b);
+	printf("\033[36mvaleurs attendue :\033[39m\na = 1\nb = 7\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\na = %d\nb = %d\n", a, b);
 
 	// test 03
 	printf("\033[96m====== TEST ft_swap(03) ======\033[39m\n");
-	div = 23; mod = 3;
-	ft_swap(&div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 3\nmod = 23\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
+	a = 23; b = 3;
+	ft_swap(&a, &b);
+	printf("\033[36mvaleurs attendue :\033[39m\na = 3\nb = 23\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\na = %d\nb = %d\n", a, b);
 
 	// test 04
 	printf("\033[96m====== TEST ft_swap(04) ======\033[39m\n");
-	div = 1; mod = 43;
-	ft_swap(&div, &mod);
-	printf("\033[36mvaleurs attendue :\033[39m\ndiv = 43\nmod = 1\n");
-	printf("\033[36mvaleurs obtenue  :\033[39m\ndiv = %d\nmod = %d\n", div, mod);
+	a = 1; b = 43;
+	ft_swap(&a, &b);
+	printf("\033[36mvaleurs attendue :\033[39m\na = 43\nb = 1\n");
+	printf("\033[36mvaleurs obtenue  :\033[39m\na = %d\nb = %d\n", a, b);
 
 	// end for tests
 	printf("\033[96m========= FIN DES TESTS =========\033[39m\n");
