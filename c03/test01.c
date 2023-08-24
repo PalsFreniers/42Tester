36f2d5df648896dc696db2db349a7100d95fdadb
Tidian Delage <tdelage@3F5.42angouleme.fr>
2023-08-24 20:43:55 +0200

    patch test01 from c03

--- c03/test01.c
+++ c03/test01.c
@@ -8,31 +8,31 @@ int main() {
 
 	// test 01
 	printf("\033[96m====== TEST ft_strncmp(01) ======\033[39m\n");
-	len = strncmp("Hello, world!", "Hello, dragster!", 7);
+	len = ft_strncmp("Hello, world!", "Hello, dragster!", 7);
 	printf("\033[36mvaleurs attendue :\033[39m\n0\n");
 	printf("\033[36mvaleurs obtenue  :\033[39m\n%d\n", len);
 
 	// test 02
 	printf("\033[96m====== TEST ft_strncmp(02) ======\033[39m\n");
-	len = strncmp("Hello, world!", "Hello, World!", 10);
+	len = ft_strncmp("Hello, world!", "Hello, World!", 10);
 	printf("\033[36mvaleurs attendue :\033[39m\n(any non null positive value)\n");
 	printf("\033[36mvaleurs obtenue  :\033[39m\n%d\n", len);
 
 	// test 03
 	printf("\033[96m====== TEST ft_strncmp(03) ======\033[39m\n");
-	len = strncmp("Hello, World!", "Hello, world!", 10);
+	len = ft_strncmp("Hello, World!", "Hello, world!", 10);
 	printf("\033[36mvaleurs attendue :\033[39m\n(any non null negative value)\n");
 	printf("\033[36mvaleurs obtenue  :\033[39m\n%d\n", len);
 
 	// test 04
 	printf("\033[96m====== TEST ft_strncmp(04) ======\033[39m\n");
-	len = strncmp("Hello, World!", "Hello, ", 8);
+	len = ft_strncmp("Hello, World!", "Hello, ", 8);
 	printf("\033[36mvaleurs attendue :\033[39m\n(any non null positive value)\n");
 	printf("\033[36mvaleurs obtenue  :\033[39m\n%d\n", len);
 
 	// test 05
 	printf("\033[96m====== TEST ft_strncmp(05) ======\033[39m\n");
-	len = strncmp("Hello, W", "Hello, Worldium", 12);
+	len = ft_strncmp("Hello, W", "Hello, Worldium", 12);
 	printf("\033[36mvaleurs attendue :\033[39m\n(any non null negative value)\n");
 	printf("\033[36mvaleurs obtenue  :\033[39m\n%d\n", len);
 
